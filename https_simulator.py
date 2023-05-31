368db445777249f7e9880c6da4cfb90e6bf5084a
wrfrohlich <wrfrohlich@hotmail.com>
2023-05-30 23:21:14 -0300

    Add static values
    FEATURE: Add static values

--- https_simulator.py
+++ https_simulator.py
@@ -22,14 +22,57 @@ class HttpsSimulator():
                                         "D662A4D18E73AFA32D779D5918D08BC8",
                                         "858F4DCEF97C2A24855E6EEB22B3B2E5")
 
+        self.a = "%s%s%s%s%s%s%s%s" % ( "d7ea9337c772e836e5113f8bb89f6629",
+                                        "822ba3efb5bef3dcf4727b0f964bcbbc",
+                                        "6b503fbeceab1aa545a7f64d2af406db",
+                                        "b36923486aff1304d4f69d294d04f8cf",
+                                        "cafdf1d9725aa735e633c28b84e62935",
+                                        "a3be3993a4c987467a64be39372892b5",
+                                        "977c89c83fe11b22f882e2175d17dc6b",
+                                        "86fbb91e7873c0d132c8757675")
+
+        self.A = "%s%s%s%s%s%s%s%s" % ( "7020D292C8173E88F90EB6656133CA33",
+                                        "DB931DBDE43AFE844D8CFFCC50947363",
+                                        "AA8530FD429C3F29ED38C91DDDBFC1FB",
+                                        "7D239573EBC372331AA89292B2E12AB6",
+                                        "8FECA44D720CC5CFB2CEA495AF2A23C9",
+                                        "D598862ABA9E33EE4AA88F6922B46ED9",
+                                        "6899DFDDD76D41B7117E84250AD37770",
+                                        "1BF82351FBFFE6021D461B095C4A4496")
+
+        self.B = "%s%s%s%s%s%s%s%s" % ( "6465FF74501E724583AE76A9E0DCCBDF",
+                                        "BE4A646DF0193A42031B3F958EEE4FF7",
+                                        "A9AA6A1C8A90F52F5C3FC76D0670C71D",
+                                        "ABA32849FE89DD2DDCE11B15CD790C97",
+                                        "8ADD7340CDD9CFBB738580BD01204C60",
+                                        "4FDFFA220DC25515D461E7CAC5A7DF20",
+                                        "1B9F358D7EA2DDAE2B8FCA64FB242F1C",
+                                        "D89DDA853F89D9CFE45B9CDA285C6952")
+
+        self.MSG = "%s%s%s%s%s%s%s" %("ABBDE81FFF5F4FC4740A5BD391B441CB",
+                                        "2BC4A45FDCFFF4EA73C19C5E5804C5F9",
+                                        "8DF745F74AC71E366B695932A88DF44F",
+                                        "B7A95DB96BE4989A37FE68D12AF2EB84",
+                                        "730A5A183EFD4F59622F6909C64E8043",
+                                        "4C1D93DC79FEB7D8CFB309CED2A4FF23",
+                                        "F090952BBC9CC28DCAF1677FBB3291DF")
+
     def generate_A(self, a = None):
         a = self.generate_random_a()
         a = int(a)
-        p = int(self.p, 16)
         g = int(self.g, 16)
+        p = int(self.p, 16)
         A_dec = pow(g, a, p)
         A_hex = hex(A_dec)[2:].upper()
         return a, A_dec, A_hex
+
+    def generate_V(self):
+        a = int(self.a, 16)
+        B = int(self.B, 16)
+        p = int(self.p, 16)
+        V_dec = pow(B, a, p)
+        V_hex = hex(V_dec)[2:].upper()
+        return V_dec, V_hex
     
     def generate_random_a(self):
         a = 0
@@ -58,4 +101,4 @@ if __name__ == '__main__':
             https_simulator.report(argv[1], "A (hex): %s" % (ret[2]))
             https_simulator.report(argv[1], "\n")
         else:
-            pass
+            print(https_simulator.generate_V())
